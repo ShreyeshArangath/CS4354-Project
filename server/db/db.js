7e18f9ac0336df9e6f297c24ef2addf6611d9514
Shreyesh Arangath <shreyesh@Shreyeshs-MacBook-Pro.local>
2022-04-05 10:08:40 -0500

    established connection with MYSQL

---
+++ server/db/db.js
@@ -0,0 +1,29 @@
+class DBHelper{
+    constructor(mysql, host, user, password, database) {
+     this.connection = mysql.createConnection({
+        host     : host,
+        user     : user,
+        password : password,
+        database : database
+      });
+    }
+
+    establishConnection() {
+        this.connection.connect((err) => {
+            if (err) {
+                console.error("There was an issue connecting to MySQL: Error Stack: " + err.stack)
+                return
+            }
+            console.log('Connected with to MYSQL with Thread ID: ' + this.connection.threadId);
+
+          });
+    }
+
+
+    stopConnection() {
+        this.connection.end();
+    }
+    
+} 
+
+module.exports = {DBHelper}
--- server/index.js
+++ server/index.js
@@ -1,6 +1,31 @@
-const express = require("express");
-const app = express();
-const PORT = 9000; //TODO: Change this to be dynamic
+//External Dependencies 
+require('dotenv').config()
+const express = require("express")
+const mysql = require('mysql'); // MySQL dependency injection 
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
+// Internal Dependencies 
+const { DBHelper } = require('./db/DB');
+const {PORT} = require("./config/dev");
+
+// Dependency Injection 
+const dbHelper = new DBHelper(
+    mysql, 
+    process.env.MYSQL_HOST, 
+    process.env.MYSQL_USER, 
+    process.env.MYSQL_PASSWORD, 
+    process.env.MYSQL_DATABASE
+    )
+
+// App Config  
+const app = express()
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Server Config 
 app.listen(9000, () => {
     console.log("Running the backend server on port 9000");
+    dbHelper.establishConnection();
 })
